601dc7133ef06cfa303f718678da0da9364d581e
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-18 19:04:29 -0300

    feat: implement imperative fibonacci algorithm

--- 3-chapter-arrays/fibonacci.ts
+++ 3-chapter-arrays/fibonacci.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+export const fibonacci = (n: number): number => {
+    const sequence = []
+    sequence[1] = 1
+    sequence[2] = 1
+
+    for (let i = 3; i <= n; i++) {
+        sequence[i] = sequence[i-1] + sequence[i-2] 
+    }
+
+    return sequence[n]
+}
