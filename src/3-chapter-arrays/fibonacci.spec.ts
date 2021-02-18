4b448c4952e0bb78d2d12a4ee83a3acf3d40a3a9
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-18 19:46:29 -0300

    test: ensure fibonacci function returns an error if negative values is provided

--- src/3-chapter-arrays/fibonacci.spec.ts
+++ src/3-chapter-arrays/fibonacci.spec.ts
@@ -1,8 +1,18 @@
 import { fibonacci } from './fibonacci'
 
 describe('Imperative Fibonacci', () => {
+  test('Should check if function returns the min value', () => {
+    const result = fibonacci(0)
+    expect(result).toBe(0)
+  })
+
   test('Should check if fibonacci returns correct value', () => {
     const result = fibonacci(10)
     expect(result).toBe(55)
+  })
+
+  test('Should return an error if a negative value is provided', () => {
+    const result = fibonacci(-1)
+    expect(result).toEqual(new Error('The value must be positive'))
   })
 })
