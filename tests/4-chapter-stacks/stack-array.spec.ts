e8a0128b679e83f91ae91518d6b2b3cf3ad03b98
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-20 10:06:04 -0300

    test: ensure StackArray add elements on top of the stack

--- tests/4-chapter-stacks/stack-array.spec.ts
+++ tests/4-chapter-stacks/stack-array.spec.ts
@@ -14,4 +14,11 @@ describe('Stack Array', () => {
     sut.push(1)
     expect(fakeData).toContain(1)
   })
+
+  test('Should add elements on top of the stack', () => {
+    const fakeData: [] = []
+    const sut = makeSut(fakeData)
+    sut.push(1, 2, 3)
+    expect(fakeData).toEqual([1, 2, 3])
+  })
 })
