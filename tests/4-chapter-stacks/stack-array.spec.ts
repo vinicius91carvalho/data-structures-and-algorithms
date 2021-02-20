9ed4faff0180afd360de00d58a686200e5e59731
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-20 10:29:24 -0300

    test: ensure StackArray pop method removes the last element of the array

--- tests/4-chapter-stacks/stack-array.spec.ts
+++ tests/4-chapter-stacks/stack-array.spec.ts
@@ -1,19 +1,30 @@
 import { StackArray } from '@/4-chapter-stacks/stack-array'
 
-const makeSut = (initialData: []): StackArray => new StackArray(initialData)
+const makeSut = (initialData: any[]): StackArray => new StackArray(initialData)
 
 describe('Stack Array', () => {
-  test('Should add a new element on top of the stack', () => {
-    const fakeData: [] = []
-    const sut = makeSut(fakeData)
-    sut.push(1)
-    expect(fakeData).toContain(1)
+  describe('push()', () => {
+    test('Should add a new element on top of the stack', () => {
+      const fakeData: [] = []
+      const sut = makeSut(fakeData)
+      sut.push(1)
+      expect(fakeData).toContain(1)
+    })
+
+    test('Should add elements on top of the stack', () => {
+      const fakeData: [] = []
+      const sut = makeSut(fakeData)
+      sut.push(1, 2, 3)
+      expect(fakeData).toEqual([1, 2, 3])
+    })
   })
 
-  test('Should add elements on top of the stack', () => {
-    const fakeData: [] = []
-    const sut = makeSut(fakeData)
-    sut.push(1, 2, 3)
-    expect(fakeData).toEqual([1, 2, 3])
+  describe('pop()', () => {
+    test('Should remove an element on top of the stack', () => {
+      const fakeData: number[] = [1, 2, 3, 4]
+      const sut = makeSut(fakeData)
+      sut.pop()
+      expect(fakeData).toEqual([1, 2, 3])
+    })
   })
 })
