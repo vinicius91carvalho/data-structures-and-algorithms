c90806aaaf4e56f1e4998e978dace073aba6c08f
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-20 09:56:50 -0300

    test: ensure StackArray can be created without an array passed as a parameter

--- tests/4-chapter-stacks/stack-array.spec.ts
+++ tests/4-chapter-stacks/stack-array.spec.ts
@@ -1,12 +1,17 @@
 import { StackArray } from '@/4-chapter-stacks/stack-array'
 
-const makeSut = (data: []): StackArray => new StackArray(data)
+const makeSut = (data?: []): StackArray => new StackArray(data)
 
 describe('Stack Array', () => {
+  test('Should create a new StackArray without a external data', () => {
+    const sut = makeSut()
+    expect(sut).toBeTruthy()
+  })
+
   test('Should add a new element on top of the stack', () => {
-    const data: [] = []
-    const sut = makeSut(data)
+    const fakeData: [] = []
+    const sut = makeSut(fakeData)
     sut.push(1)
-    expect(data).toContain(1)
+    expect(fakeData).toContain(1)
   })
 })
