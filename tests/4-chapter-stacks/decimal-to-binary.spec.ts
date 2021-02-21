40b52fa04d05537045fd3f67358b944ca1b55c1a
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-21 09:17:26 -0300

    test: ensure DecimalToBinary calcQuotient method returns 5 when value 10 is provided

--- tests/4-chapter-stacks/decimal-to-binary.spec.ts
+++ tests/4-chapter-stacks/decimal-to-binary.spec.ts
@@ -2,16 +2,24 @@ import { DecimalToBinary } from '@/4-chapter-stacks/decimal-to-binary'
 
 describe('DecimalToBinary', () => {
   describe('calcRest()', () => {
-    test('Should returns 0 when an even value is provided', () => {
+    test('Should return 0 when an even value is provided', () => {
       const decimalToBinary = new DecimalToBinary()
       const rest = decimalToBinary.calcRest(10)
       expect(rest).toBe(0)
     })
 
-    test('Should returns 0 when an odd value is provided', () => {
+    test('Should return 1 when an odd value is provided', () => {
       const decimalToBinary = new DecimalToBinary()
       const rest = decimalToBinary.calcRest(5)
       expect(rest).toBe(1)
+    })
+  })
+
+  describe('calcQuotient()', () => {
+    test('Should return the quotient correctly for the number 10', () => {
+      const decimalToBinary = new DecimalToBinary()
+      const quotient = decimalToBinary.calcQuotient(10)
+      expect(quotient).toBe(5)
     })
   })
 })
