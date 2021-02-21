dd14b4e1be67471d6ea311a1d7c2ea7d2973e156
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-21 11:46:29 -0300

    feat: ensure DecimalToBinary calc method fill Stack with correct values

--- src/4-chapter-stacks/decimal-to-binary.ts
+++ src/4-chapter-stacks/decimal-to-binary.ts
@@ -1,9 +1,22 @@
+import { PushItems } from '@/4-chapter-stacks/stacks/stack-protocols'
+
 export class DecimalToBinary {
+  constructor (private readonly pushItems: PushItems) {}
+
   calcRest (dividend: number): number {
     return dividend % 2
   }
 
   calcQuotient (dividend: number): number {
     return Math.floor(dividend / 2)
+  }
+
+  calc (value: number): void {
+    let dividend = value
+    while (dividend > 0) {
+      const rest = this.calcRest(dividend)
+      this.pushItems.push(rest)
+      dividend = this.calcQuotient(dividend)
+    }
   }
 }
