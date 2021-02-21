bf98624d1dbb56e6e48f1c0e7abdeeb0ab6a0ea9
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-21 09:12:47 -0300

    test: ensure DecimalToBinary calcRest method returns 1 when an odd value is provided

--- tests/4-chapter-stacks/decimal-to-binary.spec.ts
+++ tests/4-chapter-stacks/decimal-to-binary.spec.ts
@@ -7,5 +7,11 @@ describe('DecimalToBinary', () => {
       const rest = decimalToBinary.calcRest(10)
       expect(rest).toBe(0)
     })
+
+    test('Should returns 0 when an odd value is provided', () => {
+      const decimalToBinary = new DecimalToBinary()
+      const rest = decimalToBinary.calcRest(5)
+      expect(rest).toBe(1)
+    })
   })
 })
