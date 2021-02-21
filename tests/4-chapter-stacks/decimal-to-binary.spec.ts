dd50ef1aa422c2d8d0f5c0a03a55de1dfec11d92
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-21 09:23:34 -0300

    refactor: move sut creation to a factory method

--- tests/4-chapter-stacks/decimal-to-binary.spec.ts
+++ tests/4-chapter-stacks/decimal-to-binary.spec.ts
@@ -1,30 +1,38 @@
 import { DecimalToBinary } from '@/4-chapter-stacks/decimal-to-binary'
 
+interface SutTypes {
+  sut: DecimalToBinary
+}
+
+const makeSut = (): SutTypes => ({
+  sut: new DecimalToBinary()
+})
+
 describe('DecimalToBinary', () => {
   describe('calcRest()', () => {
     test('Should return 0 when an even value is provided', () => {
-      const decimalToBinary = new DecimalToBinary()
-      const rest = decimalToBinary.calcRest(10)
+      const { sut } = makeSut()
+      const rest = sut.calcRest(10)
       expect(rest).toBe(0)
     })
 
     test('Should return 1 when an odd value is provided', () => {
-      const decimalToBinary = new DecimalToBinary()
-      const rest = decimalToBinary.calcRest(5)
+      const { sut } = makeSut()
+      const rest = sut.calcRest(5)
       expect(rest).toBe(1)
     })
   })
 
   describe('calcQuotient()', () => {
     test('Should return the quotient correctly for the number 10', () => {
-      const decimalToBinary = new DecimalToBinary()
-      const quotient = decimalToBinary.calcQuotient(10)
+      const { sut } = makeSut()
+      const quotient = sut.calcQuotient(10)
       expect(quotient).toBe(5)
     })
 
     test('Should return the quotient correctly for the number 5', () => {
-      const decimalToBinary = new DecimalToBinary()
-      const quotient = decimalToBinary.calcQuotient(5)
+      const { sut } = makeSut()
+      const quotient = sut.calcQuotient(5)
       expect(quotient).toBe(2)
     })
   })
