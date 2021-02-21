ccc65c109d284afdb3edbd08ff729c9f0de02a7f
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-21 09:09:35 -0300

    test: ensure DecimalToBinary calcRest method returns 0 when an even value is provided

---
+++ tests/4-chapter-stacks/decimal-to-binary.spec.ts
@@ -0,0 +1,11 @@
+import { DecimalToBinary } from '@/4-chapter-stacks/decimal-to-binary'
+
+describe('DecimalToBinary', () => {
+  describe('calcRest()', () => {
+    test('Should returns 0 when an even value is provided', () => {
+      const decimalToBinary = new DecimalToBinary()
+      const rest = decimalToBinary.calcRest(10)
+      expect(rest).toBe(0)
+    })
+  })
+})
