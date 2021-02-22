7132d83023eacb5016b1e6eac3b5d1ad7758454c
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-22 16:22:01 -0300

    feat: ensure DecimalToAnotherBase is built with base 36

--- tests/unit/algoritms/decimal-to-another-base.unit.ts
+++ tests/unit/algoritms/decimal-to-another-base.unit.ts
@@ -5,12 +5,17 @@ import faker from 'faker'
 const makeSut = (base: number): DecimalToAnotherBase => DecimalToAnotherBase.toBase(base)
 
 describe('DecimalToAnotherBase', () => {
-  test('Should make a GeneralDecimalConverter with base from 2', () => {
+  test('Should make a DecimalToAnotherBase with base 2', () => {
     const sut = makeSut(2)
-    expect(sut).toBeTruthy()
+    expect(sut instanceof DecimalToAnotherBase).toBe(true)
   })
 
-  test('Should not make a GeneralDecimalConverter with base less than 2', () => {
+  test('Should not make a DecimalToAnotherBase with base less than 2', () => {
     expect(() => { makeSut(faker.random.number(1)) }).toThrowError(new IllegalArgumentError('toBase', 'base'))
+  })
+
+  test('Should make a DecimalToAnotherBase with base 36', () => {
+    const sut = makeSut(36)
+    expect(sut instanceof DecimalToAnotherBase).toBe(true)
   })
 })
