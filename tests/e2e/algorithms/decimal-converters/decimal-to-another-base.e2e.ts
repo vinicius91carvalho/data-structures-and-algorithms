a136d1504869ecdc0a3bd1a878b428a2c8b865ed
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-22 19:52:36 -0300

    (e2e) test: ensure DecimalToAnotherBase returns correct value when base binary is provided

---
+++ tests/e2e/algorithms/decimal-converters/decimal-to-another-base.e2e.ts
@@ -0,0 +1,12 @@
+import { DecimalToAnotherBase } from '@/algorithms/decimal-converters/decimal-to-another-base'
+import { makeDecimalToAnotherBase } from '@/factories/algorithms/decimal-to-another-base-factory'
+
+const makeSut = (base: number): DecimalToAnotherBase => makeDecimalToAnotherBase(base)
+
+describe('DecimalToAnotherBase', () => {
+  test('Should return the correct value when binary base is used', () => {
+    const sut = makeSut(2)
+    const convertedValue = sut.convert(100345)
+    expect(convertedValue).toEqual('11000011111111001')
+  })
+})
