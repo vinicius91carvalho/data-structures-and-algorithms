b47736f0bb067024fd5f4f020e5da270c36d02cc
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-22 19:11:33 -0300

    feat: ensure DecimalToAnotherBase convert method calls pop on stack with correct values

--- src/algorithms/decimal-converters/decimal-to-another-base.ts
+++ src/algorithms/decimal-converters/decimal-to-another-base.ts
@@ -1,13 +1,17 @@
 import { CalcQuotient, CalcRest, ConvertDecimal } from '@/algorithms/decimal-converters/decimal-converter'
-import { PushItems } from '@/data-structures/stacks/stack-protocols'
+import { PopItem, PushItems } from '@/data-structures/stacks/stack-protocols'
 import { IllegalArgumentError } from '@/errors/illegal-argument-error'
 
 export class DecimalToAnotherBase implements CalcRest, CalcQuotient, ConvertDecimal {
+  readonly MIN_BASE = 2
+  readonly MAX_BASE = 36
+
   constructor (
     private readonly base: number,
-    private readonly pushItems: PushItems
+    private readonly pushItems: PushItems,
+    private readonly popItem: PopItem
   ) {
-    if (base < 2 || base > 36) {
+    if (base < this.MIN_BASE || base > this.MAX_BASE) {
       throw new IllegalArgumentError('toBase', 'base')
     }
   }
@@ -26,6 +30,10 @@ export class DecimalToAnotherBase implements CalcRest, CalcQuotient, ConvertDeci
       this.pushItems.push(rest)
       dividend = this.calcQuotient(dividend)
     }
+    let item: number
+    do {
+      item = this.popItem.pop()
+    } while (item !== undefined)
     return ''
   }
 }
