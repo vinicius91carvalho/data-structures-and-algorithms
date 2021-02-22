c80105688c99496941289bc93d9e075379533168
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-22 16:18:16 -0300

    test: ensure DecimalToAnotherBase is not built with base less than 2

--- tests/unit/algoritms/decimal-to-another-base.unit.ts
+++ tests/unit/algoritms/decimal-to-another-base.unit.ts
@@ -1,10 +1,16 @@
 import { DecimalToAnotherBase } from '@/algorithms/decimal-to-another-base'
+import { IllegalArgumentError } from '@/errors/illegal-argument-error'
+import faker from 'faker'
 
-const makeSut = (base: number): DecimalToAnotherBase => DecimalToAnotherBase.toBase(2)
+const makeSut = (base: number): DecimalToAnotherBase => DecimalToAnotherBase.toBase(base)
 
 describe('DecimalToAnotherBase', () => {
   test('Should make a GeneralDecimalConverter with base from 2', () => {
     const sut = makeSut(2)
     expect(sut).toBeTruthy()
+  })
+
+  test('Should not make a GeneralDecimalConverter with base less than 2', () => {
+    expect(() => { makeSut(faker.random.number(1)) }).toThrowError(new IllegalArgumentError('toBase', 'base'))
   })
 })
