13061ff73fa65661b78621f37671009a7cfcc170
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-23 11:34:42 -0300

    refactor: replace static data for faker random data on queue unit test

--- tests/unit/data-structures/queues/queue-array.unit.ts
+++ tests/unit/data-structures/queues/queue-array.unit.ts
@@ -1,11 +1,19 @@
 import { QueueArray } from '@/data-structures/queue/queue-array'
+import faker from 'faker'
+
+const makeFakeItems = (): any[] => ([
+  faker.random.objectElement(),
+  faker.random.word(),
+  faker.random.number()
+])
 
 describe('QueueArray', () => {
   describe('enqueue()', () => {
     test('Should add elements on queue', () => {
+      const items = makeFakeItems()
       const queue = new QueueArray()
-      queue.enqueue(1, 2, 3, 4, 5)
-      expect(queue.getItems()).toEqual([1, 2, 3, 4, 5])
+      queue.enqueue(...items)
+      expect(queue.getItems()).toEqual(items)
     })
   })
 })
