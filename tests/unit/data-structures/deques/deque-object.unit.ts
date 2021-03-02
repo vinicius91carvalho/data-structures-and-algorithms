5f062cdb34361e83323e3da8d24fb388de35fa87
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-03-02 19:38:08 -0300

    refactor: move sut creation to a factory method

--- tests/unit/data-structures/deques/deque-object.unit.ts
+++ tests/unit/data-structures/deques/deque-object.unit.ts
@@ -1,16 +1,19 @@
 import { DequeObject } from '@/data-structures/deques/deque-object'
+import { Deque } from '@/data-structures/deques/deque-protocols'
+
+const makeSut = (elements? : any[]): Deque => new DequeObject(elements)
 
 describe('DequeObject', () => {
   describe('getItems()', () => {
     test('Should get initialized elements', () => {
       const elements = [1, 2, 3]
-      const deque = new DequeObject(elements)
+      const deque = makeSut(elements)
       expect(deque.getItems()).toEqual(elements)
     })
 
     test('Should not get the same reference', () => {
       const elements = [1, 2, 3]
-      const deque = new DequeObject(elements)
+      const deque = makeSut(elements)
       expect(deque.getItems()).not.toBe(elements)
     })
   })
