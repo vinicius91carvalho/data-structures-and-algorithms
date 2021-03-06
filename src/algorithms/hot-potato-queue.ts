193fff765a34850713f60fce57126c4c425642b7
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-03-06 12:29:00 -0300

    feat: ensure HotPotato pass the potato to the another participant based on the number of steps

--- src/algorithms/hot-potato-queue.ts
+++ src/algorithms/hot-potato-queue.ts
@@ -1,9 +1,18 @@
-import { EnqueueItems } from '@/data-structures/queues/queue-protocols'
+import { DequeueItem, EnqueueItems } from '@/data-structures/queues/queue-protocols'
 
 export class HotPotatoQueue {
   constructor (
     participants: string[],
-    private readonly enqueueItems: EnqueueItems) {
-    this.enqueueItems.enqueue(...participants)
+    private readonly enqueueItems: EnqueueItems,
+    private readonly dequeueItem: DequeueItem
+  ) {
+    this.enqueueItems.enqueue(participants)
+  }
+
+  play (steps: number): void {
+    while (steps > 0) {
+      this.enqueueItems.enqueue(this.dequeueItem.dequeue())
+      steps--
+    }
   }
 }
