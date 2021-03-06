e6f1ff661f4386809af93e85b20ac86b41b16519
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-03-06 11:20:15 -0300

    test: ensure HotPotato does not allow that participants will be change outside

--- tests/unit/algoritms/hot-potato.unit.ts
+++ tests/unit/algoritms/hot-potato.unit.ts
@@ -6,4 +6,11 @@ describe('HotPotato', () => {
     const hotPotato = new HotPotato(participants)
     expect(hotPotato.participants).toEqual(participants)
   })
+
+  test('Should not permit that participants will be change outside', () => {
+    const participants = ['João', 'José', 'Maria', 'Pedro']
+    const hotPotato = new HotPotato(participants)
+    hotPotato.participants = []
+    expect(hotPotato.participants).toEqual(participants)
+  })
 })
